485d514f67ea3956b681f8c2518eac3eaf3c7f53
Anwesha <anwesha09jsr@gmail.com>
2026-05-23 16:06:08 +0530

    Day 1 :Setup + hello.py complete

---
+++ Month1/week1/hello.py
@@ -0,0 +1,19 @@
+#Week 1 Day 1 - First Python program
+name="Anwesha"
+college="Arka Jain university"
+print (f"Hello {name}")
+print (f"Starting BCA AI prep for {college}")
+print ("Goal: Top 1% before Day 1")
+
+#Basic 1:Variables
+age = 18 
+bca_year= 2026 
+#Basic 2:Math 
+days_left = 90 
+hours_per_day = 1 
+total_hours = days_left * hours_per_day
+#Basic 3: f-strings
+print (f"\nAge:{age}")
+print (f"BCA Batch:{bca_year}")
+print(f"Total prep time:{total_hours} hours")
+print (f"Daily commit needed: {hours_per_day} hour/day")
